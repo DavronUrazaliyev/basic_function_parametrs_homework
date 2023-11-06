4b2192b034328c53e30d8a00ca28c0d75bbc8c21
Davron17 <dorazaliyev75@gmail.com>
2023-11-06 23:53:33 +0500

    minimum

--- basic_function_parameter03.py
+++ basic_function_parameter03.py
@@ -1,3 +1,11 @@
 # Create a function named find_smallest that takes a list of numbers as a parameter.
 # Inside the function, find the smallest number in the given list.
 # Return the smallest number.
+def find_smallest():
+    l=[1,2,3,4,5]
+    min=l[0]
+    for i in l:
+        if i<min:
+            min=i
+    return min
+print(find_smallest())
